4d16c6295664945930f3dd2e66f4376436dd0ac4
sjackson@drakeent.com <sjackson@drakeent.com>
2016-10-31 09:45:45 -0400

    Cancel old toast to speed up notification.

--- PiCar/PiCar/PiCar.Droid/Notifier.cs
+++ PiCar/PiCar/PiCar.Droid/Notifier.cs
@@ -7,10 +7,15 @@ namespace PiCar.Droid
 {
     public class Notifier : INotifier
     {
+        public static Toast toast;
+
         public Notifier() { }
 
-        public void MakeToast(string message)
-            => Device.BeginInvokeOnMainThread(()
-                => Toast.MakeText(Forms.Context, message, ToastLength.Short).Show());
+        public void MakeToast(string message) => Device.BeginInvokeOnMainThread(() =>
+        {
+            toast?.Cancel();
+            toast = Toast.MakeText(Forms.Context, message, ToastLength.Short);
+            toast.Show();
+        });
     }
 }
